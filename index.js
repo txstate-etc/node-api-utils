306389c1394a155812e135a594523eeca88815f7
wickning1 <git@mythgaming.net>
2019-02-04 09:25:44 -0600

    avoid exposing service in the main export; if anyone only intends to use the regular service, they should require('node-api-utils/lib/service')

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   apiservice: require('./lib/apiservice'),
   db: require('./lib/db'),
-  util: require('./lib/util'),
-  service: require('./lib/service')
+  util: require('./lib/util')
 }
